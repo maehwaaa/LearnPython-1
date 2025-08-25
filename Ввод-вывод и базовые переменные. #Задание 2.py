cbdfcde5bbf163957f92e16075549e1a9396de95
maehwa <68327208+maehwaaa@users.noreply.github.com>
2025-08-25 18:03:11 +0500

    Ввод-вывод и базовые переменные. #Задание 2.py

--- Ввод-вывод и базовые переменные. #Задание 2.py
+++ Ввод-вывод и базовые переменные. #Задание 2.py
@@ -1,2 +1,8 @@
 #Задание 2
-print("Australopithecus", "Homo Habilis", "Homo Erectus", "Homo Neanderthalensis", "Homo Sapiens", "Homo Sapiens Sapiens", sep=' => ')
+stage1 = input("Введите первый этап развития человека: ")
+stage2 = input("Введите второй этап развития человека: ")
+stage3 = input("Введите третий этап развития человека: ")
+stage4 = input("Введите четвёртый этап развития человека: ")
+stage5 = input("Введите пятый этап развития человека: ")
+
+print(stage1, stage2, stage3, stage4, stage5, sep='=>')
